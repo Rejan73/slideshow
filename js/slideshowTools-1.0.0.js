0e89636da13848da4814ec690c6933c60b6da14d
Rejan73 <47070116+Rejan73@users.noreply.github.com>
2021-03-20 09:01:08 +0100

    Add files via upload

--- js/slideshowTools-1.0.0.js
+++ js/slideshowTools-1.0.0.js
@@ -15,7 +15,7 @@ var openFile = function(event) {
       };
 
 function fillSlideShow(){
-    var div_data='<table><th></th><th>File</th><th>Width</th><th>Height</th><th>PlayTime in s</th><th>Effect</th><th>Actions</th>';
+    var div_data='<table><th></th><th>File</th><th>Width</th><th>Height</th><th>PlayTime in s</th><th>Actions</th>';
     var cpt=0;
     slideShowDatas.forEach(slideShowData => div_data+=printSlideShowData(slideShowData,cpt++));
     div_data+='</table>';
@@ -41,7 +41,6 @@ function printSlideShowData(slideShowData,cpt){
          line+='<td><i class="fa fa-file-audio-o fa-2x" ></i></td><td>'+getFilePath(slideShowData.file)+'</td><td></td><td></td>';
          line+='<td>start <input type="text" size="1" id="startFile'+cpt+'" value="' + getStartTime(slideShowData.file) +'"/>';
          line+=' end <input type="text" size="1" id="endFile'+cpt+'" value="' + getEndTime(slideShowData.file)+'"/></td>';
-         line+='<td>'+getEffectMusic(cpt,slideShowData.effect)+'</td>';
          break;
     case "mp4":
          line+='<td><i class="fa fa-file-movie-o fa-2x" ></i></td><td>'+getFilePath(slideShowData.file)+'</td>'
@@ -49,67 +48,36 @@ function printSlideShowData(slideShowData,cpt){
          line+='<td><input type="text" size="1" id="heightFile'+cpt+'" value="' + slideShowData.height+'"/></td>';
          line+='<td>start <input type="text" size="1" id="startFile'+cpt+'" value="' + getStartTime(slideShowData.file) +'"/>';
          line+=' end <input type="text" size="1" id="endFile'+cpt+'" value="' + getEndTime(slideShowData.file)+'"/></td>';
-         line+='<td>'+getEffectVideo(cpt,slideShowData.effect)+'</td>';
          break;
     case "img":
          line+='<td><i class="fa fa-file-photo-o fa-2x" ></i></td><td>'+slideShowData.file+'</td>'
          line+='<td><input type="text" size="1" id="widthFile'+cpt+'" value="' + slideShowData.width+'"/></td>';
          line+='<td><input type="text" size="1" id="heightFile'+cpt+'" value="' + slideShowData.height+'"/></td>';
          line+='<td>duration <input type="text" size="1" id="durationFile'+cpt+'" value="' + slideShowData.duration +'"/></td>';
-         line+='<td>'+getEffectImg(cpt,slideShowData.effect)+'</td>';
          break;
     case "txt":
-         line+='<td><i class="fa fa-file-text-o fa-2x" ></i></td><td>texte</td><td></td><td></td>';
+         line+='<td><i class="fa fa-file-text-o fa-2x" ></i></td><td>texte</td>';
+         line+='<td colspan="2"><a class="js-open-modal btn" href="#" title="Modify Text" onclick="updateTextAnimation('+cpt+');"><i class="fa fa-pencil-square fa-2x" ></i></a></td>';
          line+='<td>duration <input type="text" size="1" id="durationFile'+cpt+'" value="' + slideShowData.duration +'"/></td>';
-         line+='<td>'+getEffectText(cpt,slideShowData.effect)+'</td>';
          break; 
     defaut:
          break;    
   }
   
   line+='<td>';
-  if (slideShowData.media=='txt'){
-      line+='<a class="js-open-modal btn" href="#" save-media="save-media" title="Modify" onclick="updateTextAnimation('+cpt+');"><i class="fa fa-pencil-square fa-2x" ></i></a>';
-  }
-  line+='<a class="js-open-modal btn" href="#" save-media="save-media" title="Save" onclick="saveAnimation('+cpt+');"><i class="fa fa-save fa-2x" ></i></a>';
-  line+='<a class="js-open-modal btn" href="#" delete-media="remove-media" title="Remove" onclick="removeAnimation('+cpt+');"><i class="fa fa-times-circle fa-2x" style="color:red;"></i></a>';
-  line+='<a class="js-open-modal btn" href="#" moveup-media="moveup-media" title="up" onclick="moveUpAnimation('+cpt+');"><i class="fa fa-arrow-circle-up fa-2x" ></i></a>';
-  line+='<a class="js-open-modal btn" href="#" movedown-media="movedown-media" title="down" onclick="moveDownAnimation('+cpt+');"><i class="fa fa-arrow-circle-down fa-2x" ></i></a>';
-  line+='<a class="js-open-modal btn" href="#" save-media="play-media" title="Play" onclick="runAnimation('+cpt+');"><i class="fa fa-play-circle fa-2x" ></i></a></td>';
+  line+='<a class="js-open-modal btn" href="#" title="up" onclick="moveUpAnimation('+cpt+');"><i class="fa fa-arrow-circle-up fa-2x" ></i></a>';
+  line+='<a class="js-open-modal btn" href="#" title="down" onclick="moveDownAnimation('+cpt+');"><i class="fa fa-arrow-circle-down fa-2x" ></i></a>';
+  line+='<a class="js-open-modal btn" href="#" title="Modify Effect" onclick="updateEffectAnimation('+cpt+');"><i class="fa fa-film fa-2x" ></i></a>';
+  line+='<a class="js-open-modal btn" href="#" title="Save" onclick="saveAnimation('+cpt+');"><i class="fa fa-save fa-2x" ></i></a>';
+  line+='<a class="js-open-modal btn" href="#" title="Remove" onclick="removeAnimation('+cpt+');"><i class="fa fa-times-circle fa-2x" style="color:red;"></i></a>';
+  line+='<a class="js-open-modal btn" href="#" title="Play" onclick="runAnimation('+cpt+');"><i class="fa fa-play-circle fa-2x" ></i></a></td>';
   
   
   return '<tr>'+line+'</tr>';
 }
 
- 
-  function getEffectText(cpt,effect){
-    var selectEffect='<select name="effectFile'+cpt+'" id="effectFile'+cpt+'">';
-    selectEffect+='<option value="default">default</option>';
-    selectEffect+='<option value="starwars" selected>starwars</option>';
-    selectEffect+='</select>';
-    return selectEffect;
-  }
-  
-  function getEffectImg(cpt,effect){
-    var selectEffect='<select name="effectFile'+cpt+'" id="effectFile'+cpt+'">';
-    selectEffect+='<option value="default" selected>default</option>';
-    selectEffect+='</select>';
-    return selectEffect;
-  }
-  
-  function getEffectVideo(cpt,effect){
-    var selectEffect='<select name="effectFile'+cpt+'" id="effectFile'+cpt+'">';
-    selectEffect+='<option value="default" selected>default</option>';
-    selectEffect+='</select>';
-    return selectEffect;
-  }
-  
-  function getEffectMusic(cpt,effect){
-    var selectEffect='<select name="effectFile'+cpt+'" id="effectFile'+cpt+'">';
-    selectEffect+='<option value="default" selected>default</option>';
-    selectEffect+='</select>';
-    return selectEffect;
-  }
+
+
 
 
 function getFilePath(file){
@@ -136,7 +104,8 @@ function hideAll(){
     $('#images').hide();
     $('#musics').hide();
     $('#textes').hide();
-    $('#updatetexte').hide();
+    $('#updateTexte').hide();
+    $('#updateEffect').hide();
 }
 
 function stopMusic(){
@@ -198,9 +167,12 @@ function addAnimation(){
   }
   
   if (dataToAdd["media"]=='img'){
-    dataToAdd["duration"]='3';
+    dataToAdd["duration"]='5';
   } 
-  dataToAdd["effect"]='default';
+  dataToAdd["styleEffect"]="none";
+  dataToAdd["movementEffect"]="none";
+  dataToAdd["comeInEffect"]="none";
+  dataToAdd["comeOutEffect"]="none";
   dataToAdd["width"]='800';
   dataToAdd["height"]='600'; 
   slideShowDatas.push(dataToAdd);
@@ -299,15 +271,16 @@ function playMusic(currentData){
 
 
 function  updateTextAnimation(currentDataId){
+  hideAll();
   currentData=slideShowDatas[currentDataId];
-  var div_text ='Title : <input type="text" size="50" id="updatetitle" value="'+currentData.title+'"/><br>';
+  var div_text ='<p>Text Modification<p><br>Title : <input type="text" size="50" id="updatetitle" value="'+currentData.title+'"/><br>';
   div_text+='SubTitle :<input type="text" size="50" id="updatesubtitle" value="'+currentData.subTitle+'"/><br>';
   div_text+='Text : <textarea id="updatelines" rows="10" cols="100">';
   currentData.lines.forEach(object => div_text=div_text+object.line+'\n');
   div_text+='</textarea>'; 
   div_text+='<br><a class="js-open-modal btn" href="#" save-media="save-media" title="Modify" onclick="saveTextAnimation('+currentDataId+');"><i class="fa fa-pencil-square fa-2x"></i></a></td>';
-  $("#updatetexte").html(div_text);  
-  $('#updatetexte').show();
+  $("#updateTexte").html(div_text);  
+  $('#updateTexte').show();
 
 }
 function  saveTextAnimation(currentDataId){
@@ -318,9 +291,8 @@ function  saveTextAnimation(currentDataId){
   currentData["lines"]=[];
   dataLine.forEach(object => currentData["lines"].push({"line": object }));
   
-  $('#updatetexte').hide();
+  $('#updateTexte').hide();
 }
-
 
 function playText(currentData){
   var div_data ='<div class="title">'
@@ -332,4 +304,97 @@ function playText(currentData){
   $("#divCrawl").html(div_data);                     
   $('#textes').show(); 
   
-};
+}
+
+function  updateEffectAnimation(currentDataId){
+  hideAll();
+  currentData=slideShowDatas[currentDataId];
+  var div_effect ='<p>Style Modification<p><br><label><i class="fa fa-film fa-2x"></i> Style Effect</label> ';
+  switch (currentData.media)
+  {
+    case "mp3":
+         div_effect+=getMusicEffect(currentData.styleEffect);
+         break;
+    case "mp4":
+         div_effect+=getVideoEffect(currentData.styleEffect);
+         break;
+    case "img":
+        div_effect+=getMusicEffect(currentData.styleEffect);
+         break;
+    case "txt":
+         div_effect+=getTextEffect(currentData.styleEffect);
+         break; 
+    defaut:
+         break;    
+  }
+  div_effect+='<br><label><i class="fa fa-film fa-2x"></i> Movement Effect</label> ';
+  div_effect+=getMovementEffect(currentData.movementEffect);
+  div_effect+='<br><label><i class="fa fa-film fa-2x"></i> Come In Effect</label> ';
+  div_effect+=getComeInEffect(currentData.comeInEffect);
+  div_effect+='<br><label><i class="fa fa-film fa-2x"></i> Come Out Effect</label> ';
+  div_effect+=getComeOutEffect(currentData.comeOutEffect);
+  div_effect+='<br><a class="js-open-modal btn" href="#" save-media="save-media" title="Modify" onclick="saveEffectAnimation('+currentDataId+');"><i class="fa fa-save fa-2x"></i></a></td>';
+  $("#updateEffect").html(div_effect);  
+  $('#updateEffect').show();
+
+}
+function  saveEffectAnimation(currentDataId){
+  currentData=slideShowDatas[currentDataId];
+  currentData["styleEffect"]=$('#styleEffect').val();
+  currentData["movementEffect"]=$('#movementEffect').val();
+  currentData["comeInEffect"]=$('#comeInEffect').val();
+  currentData["comeOutEffect"]=$('#comeOutEffect').val();
+  
+  $('#updateEffect').hide();
+}
+  function getMovementEffect(selectedEffect){
+    var selectEffect='<select name="movementEffect" id="movementEffect">';
+    selectEffect+='<option value="none">none</option>';
+    selectEffect+='<option value="uptodown" selected>Up To Down</option>';
+    selectEffect+='</select>';
+    return selectEffect;
+  }
+
+  function getComeInEffect(selectedEffect){
+    var selectEffect='<select name="comeInEffect" id="comeInEffect">';
+    selectEffect+='<option value="none">none</option>';
+    selectEffect+='</select>';
+    return selectEffect;
+  }
+  
+  function getComeOutEffect(selectedEffect){
+    var selectEffect='<select name="comeOutEffect" id="comeOutEffect">';
+    selectEffect+='<option value="none">none</option>';
+    selectEffect+='</select>';
+    return selectEffect;
+  }
+
+  function getImageEffect(selectedEffect){
+    var selectEffect='<select name="styleEffect" id="styleEffect">';
+    selectEffect+='<option value="none">none</option>';
+    selectEffect+='<option value="starwars" selected>starwars</option>';
+    selectEffect+='</select>';
+    return selectEffect;
+  }
+  
+  function getTextEffect(selectedEffect){
+    var selectEffect='<select name="styleEffect" id="styleEffect">';
+    selectEffect+='<option value="none">none</option>';
+    selectEffect+='<option value="starwars" selected>starwars</option>';
+    selectEffect+='</select>';
+    return selectEffect;
+  }
+  
+  function getVideoEffect(selectedEffect){
+    var selectEffect='<select name="styleEffect" id="styleEffect">';
+    selectEffect+='<option value="none" selected>none</option>';
+    selectEffect+='</select>';
+    return selectEffect;
+  }
+
+  function getMusicEffect(selectedEffect){
+    var selectEffect='<select name="styleEffect" id="styleEffect">';
+    selectEffect+='<option value="none" selected>none</option>';
+    selectEffect+='</select>';
+    return selectEffect;
+  }
